f69e1e1ddb8f4d573c77bf5390d9754a08e27ad9
Ivan Khomutov Vishnevsky <i.khomutov@udc.es>
2026-03-02 16:27:46 +0100

    Estructura inicial del backend con Spring Boot

---
+++ backend/src/test/java/com/ua/nextflat/NextflatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ua.nextflat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextflatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
